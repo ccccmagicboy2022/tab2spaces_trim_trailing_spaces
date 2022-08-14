3ed3ffd254953066eb9b79a54b959066bee65f4f
cccc <cuiwei_cv@163.com>
2022-08-14 21:47:05 +0800

    add make script

---
+++ make.bat
@@ -0,0 +1,3 @@
+pyinstaller.exe --upx-dir=.\upx-3.96-win64 --icon=.\src\c_c++_code_cleaner.ico --noconfirm --onefile --console .\src\c_c++_code_cleaner.py --version-file .\src\file_version_info.txt
+
+
