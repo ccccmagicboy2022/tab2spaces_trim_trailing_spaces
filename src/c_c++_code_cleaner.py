24f7752602772925211386845475dbbeff3b607a
cccc <cuiwei_cv@163.com>
2022-09-09 21:42:40 +0800

    add single file support and add m/py file support

--- src/c_c++_code_cleaner.py
+++ src/c_c++_code_cleaner.py
@@ -48,23 +48,32 @@ def remove_spaces_line(url):
         f.writelines(line_set)
 
 def scan_files(url):
-    file = os.listdir(url)
-    for f in file:
-        real_url = path.join(url, f)
-        if path.isfile(real_url):
-            ext = os.path.splitext(f)[-1].lower()
-            for xx in ext_set:
-                if ext == xx:
-                    print(real_url)
-                    tab2spaces(real_url)
-                    trim_trailing_spaces(real_url)
-                    remove_spaces_line(real_url)
-        elif path.isdir(real_url):
-            scan_files(real_url)
-        else:
-            pass
+    if path.isdir(url):
+        file = os.listdir(url)
+        for f in file:
+            real_url = path.join(url, f)
+            if path.isfile(real_url):
+                ext = os.path.splitext(f)[-1].lower()
+                for xx in ext_set:
+                    if ext == xx:
+                        print(real_url)
+                        tab2spaces(real_url)
+                        trim_trailing_spaces(real_url)
+                        remove_spaces_line(real_url)
+            elif path.isdir(real_url):
+                scan_files(real_url)
+            else:
+                pass
+    else:
+        ext = os.path.splitext(url)[-1].lower()        
+        for xx in ext_set:
+            if ext == xx:
+                print(url)
+                tab2spaces(url)
+                trim_trailing_spaces(url)
+                remove_spaces_line(url)
 
-ext_set = ['.c', '.h', '.cpp', '.hpp']
+ext_set = ['.c', '.h', '.cpp', '.hpp', '.py', '.m']
 replacement_spaces = ' ' * 4
 __version__ = "1.0.0"
 install()
@@ -75,12 +84,12 @@ def version_callback(value: bool):
         raise typer.Exit()
 
 def path_callback(value: str):
-    typer.secho(f"{value} begin to process===", fg=typer.colors.BRIGHT_WHITE, bg=typer.colors.GREEN)
+    typer.secho(f"{value} begin to process ... ", fg=typer.colors.BRIGHT_WHITE, bg=typer.colors.GREEN)
     return value
         
 def main(version: Optional[bool] = typer.Option(None, "--version", '-v', callback=version_callback), path: str = typer.Option("./", '--path', '-p', prompt = "Paste your path of code", help="code path", confirmation_prompt=True, callback=path_callback)):
     """
-    Simple program that cleanup your c/c++ code with utf-8.
+    Simple program that cleanup your c/c++/python/matlab code with utf-8.
     """
     scan_files(path)
 
